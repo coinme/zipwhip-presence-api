13d36b0a040c8786e58c13196f53b01ea12c51e3
Jeremy Dinsel <jdinsel@zipwhip.com>
2011-11-04 11:38:00 -0700

    Copy Presence for zipwhip-api to access

--- src/main/java/com/zipwhip/signals/presence/Presence.java
+++ src/main/java/com/zipwhip/signals/presence/Presence.java
@@ -6,136 +6,188 @@ import java.util.Date;
 import com.zipwhip.signals.address.ClientAddress;
 
 /**
+ * 
  * Created by IntelliJ IDEA.
+ * 
  * User: Michael
+ * 
  * Date: 6/28/11
+ * 
  * Time: 2:43 PM
  */
+
 public class Presence implements Serializable {
 
 	// we control the serialisation version
 	private static final long serialVersionUID = 10375439476839415L;
 
+	/**
+	 * 
+	 * A protected constructor for use by the builder.
+	 * 
+	 * @param builder
+	 *            The builder from which to construct this Presence
+	 */
+
+	protected Presence(PresenceBuilder builder) {
+
+		ip = builder.ip;
+		address = builder.address;
+		category = builder.category;
+		userAgent = builder.userAgent;
+		status = builder.status;
+		connected = builder.connected;
+		subscriptionId = builder.subscriptionId;
+		lastActive = builder.lastActive;
+		extraInfo = builder.extraInfo;
+	}
+
+	/**
+	 * 
+	 * IP address of the client
+	 */
 	private String ip;
 
 	/**
+	 * 
 	 * A way to uniquely call you
 	 */
 	private ClientAddress address;
 
 	/**
+	 * 
 	 * Tablet, Phone, Browser, none
 	 */
 	private PresenceCategory category = PresenceCategory.NONE;
 
 	/**
+	 * 
 	 * Some user agent string like a browser, that tells all apps installed and versions of apps.
 	 */
 	private UserAgent userAgent;
 
 	/**
-	 * Status - online, busy, away, invisible, offline
+	 * 
+	 * Status online, busy, away, invisible, offline
 	 */
-
 	private PresenceStatus status;
 
 	/**
+	 * 
 	 * Connected
 	 */
 	private Boolean connected;
 
 	/**
+	 * 
 	 * The subscriptionId
 	 */
 	private String subscriptionId;
 
-	/*
+	/**
+	 * 
 	 * Last active Date+Time
 	 */
 	private Date lastActive;
 
+	/**
+	 * 
+	 * A way to add undefined key/value data.
+	 */
 	private PresenceExtraInfo extraInfo;
 
 	public Presence() {
 
 	}
 
-	public PresenceStatus getStatus() {
+	public PresenceStatus getStatus()
+	{
 		return status;
 	}
 
-	public void setStatus(PresenceStatus status) {
+	public void setStatus(PresenceStatus status)
+	{
 		this.status = status;
 	}
 
-	public Boolean getConnected() {
+	public Boolean getConnected()
+	{
 		return connected;
 	}
 
-	public void setConnected(Boolean connected) {
+	public void setConnected(Boolean connected)
+	{
 		this.connected = connected;
 	}
 
-	public String getSubscriptionId() {
+	public String getSubscriptionId()
+	{
 		return subscriptionId;
 	}
 
-	public void setSubscriptionId(String subscriptionId) {
+	public void setSubscriptionId(String subscriptionId)
+	{
 		this.subscriptionId = subscriptionId;
 	}
 
-	public String getIp() {
+	public String getIp()
+	{
 		return ip;
 	}
 
-	public void setIp(String ip) {
+	public void setIp(String ip)
+	{
 		this.ip = ip;
 	}
 
-	public ClientAddress getAddress() {
+	public ClientAddress getAddress()
+	{
 		return address;
 	}
 
-	public void setAddress(ClientAddress address) {
+	public void setAddress(ClientAddress address)
+	{
 		this.address = address;
 	}
 
-	public PresenceCategory getCategory() {
+	public PresenceCategory getCategory()
+	{
 		return category;
 	}
 
-	public void setCategory(PresenceCategory category) {
+	public void setCategory(PresenceCategory category)
+	{
 		this.category = category;
 	}
 
-	public UserAgent getUserAgent() {
+	public UserAgent getUserAgent()
+	{
 		return userAgent;
 	}
 
-	public void setUserAgent(UserAgent userAgent) {
+	public void setUserAgent(UserAgent userAgent)
+	{
 		this.userAgent = userAgent;
 	}
 
-	/**
-	 * We clone date because returning a reference to a mutable object value stored in one of the object's fields exposes
-	 * the internal representation of the object. If instances are accessed by untrusted code, and unchecked changes to
-	 * the mutable object would compromise security or other important properties, you will need to do something different.
-	 * Returning a new copy of the object is better approach in many situations.
-	 * EI_EXPOSE_REP
-	 */
-	public Date getLastActive() {
-		return (Date) lastActive.clone();
+	public Date getLastActive()
+	{
+		return lastActive;
 	}
 
-	public void setLastActive(Date lastActive) {
-		this.lastActive = (Date) lastActive.clone();
+	public void setLastActive(Date lastActive)
+	{
+		this.lastActive = lastActive;
 	}
 
-	public PresenceExtraInfo getExtraInfo() {
+	public PresenceExtraInfo getExtraInfo()
+	{
 		return extraInfo;
 	}
 
-	public void setExtraInfo(PresenceExtraInfo extraInfo) {
+	public void setExtraInfo(PresenceExtraInfo extraInfo)
+	{
 		this.extraInfo = extraInfo;
 	}
+
 }
